dda83e6aa65537aa2a690aa214c9940cc1885980
mdsajib360 <sajib360.dev@gmail.com>
2025-08-14 10:50:33 +0600

    sidebar responsiveness

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,9 +5,36 @@ import Feed from '../Components/Feed';
 class Home extends Component {
   render() {
     return (
-      <div className="flex">
-        <Sidebar />
-        <Feed searchQuery={this.props.searchQuery} />
+      <div className="flex flex-col md:flex-row min-h-screen">
+        {/* Sidebar for Desktop */}
+        <div className="hidden md:block w-64 bg-gray-900">
+          <Sidebar />
+        </div>
+
+        {/* Feed */}
+        <div className="flex-1 p-4">
+          <Feed searchQuery={this.props.searchQuery} />
+        </div>
+
+        {/* Bottom Menu for Mobile */}
+        <div className="fixed bottom-0 left-0 right-0 md:hidden bg-gray-900 text-white flex justify-around p-2 shadow-inner">
+          <button className="flex flex-col items-center">
+            <span>🏠</span>
+            <small>Home</small>
+          </button>
+          <button className="flex flex-col items-center">
+            <span>🔥</span>
+            <small>Trending</small>
+          </button>
+          <button className="flex flex-col items-center">
+            <span>🔔</span>
+            <small>Subscriptions</small>
+          </button>
+          <button className="flex flex-col items-center">
+            <span>📺</span>
+            <small>Library</small>
+          </button>
+        </div>
       </div>
     );
   }
